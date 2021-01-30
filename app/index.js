1096e9e111a2b601664f582410c7fbc0609506a5
gideaopinheiro <gpss@ic.ufal.br>
2021-01-30 12:35:45 -0300

    feat: parses the http method

--- app/index.js
+++ app/index.js
@@ -16,11 +16,14 @@ const server = http.createServer((req, res) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  // Get the HTTP Method
+  const httpMethod = req.method.toLowerCase();
+
   // Send the response
   res.end('Hello, world!\n');
 
   // Log the request path
-  console.log('Request received on path: '+trimmedPath);
+  console.log('Request received on path: "'+trimmedPath+'" with method: '+httpMethod);
 });
 
 // Start the server, and have it listen on port 3000
